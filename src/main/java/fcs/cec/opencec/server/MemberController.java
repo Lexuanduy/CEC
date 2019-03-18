6a0e699b23e27139beb5c75314ff2840fa7cd821
lexuanduy <lexuanduy07@gmail.com>
2019-03-18 18:06:03 +0700

    update and creatting feature pagination page member list

--- src/main/java/fcs/cec/opencec/server/MemberController.java
+++ src/main/java/fcs/cec/opencec/server/MemberController.java
@@ -1,15 +1,22 @@
 package fcs.cec.opencec.server;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.Firestore;
