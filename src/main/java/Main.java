0a94f6d39e7afbb088a005b60b2ca163347a6a78
lexuanduy <lexuanduy07@gmail.com>
2019-03-27 16:33:39 +0700

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,8 @@ import java.util.concurrent.ExecutionException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.json.Json;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
@@ -22,16 +24,24 @@ public class Main {
 		// story_body_container
 //		https://www.facebook.com/hashtag/lesson8cec?source=feed_text&epa=HASHTAG
 //		https://m.facebook.com/groups/cec.edu.vn/permalink/2259370051002705/
-
-//		Document doc = Jsoup.connect("https://www.facebook.com/hashtag/lesson8cec?source=feed_text&epa=HASHTAG").get();
-//
-////		System.out.println(doc.select("meta[property=\"og:title\"]").attr("content"));
+//		String url = "https://m.facebook.com/groups/cec.edu.vn/permalink/2262271487379228/";
+//		Document doc = Jsoup.connect(url).get();
+//		System.out.println(doc.html());
+//		System.out.println(doc.select("#m_story_permalink_view .bb").attr("data-ft"));
+//		String object = doc.select("#m_story_permalink_view .bb").attr("data-ft");
+//		ObjectMapper mapper = new ObjectMapper();
+//		Map<String,Object> map = mapper.readValue(object, Map.class);
+//		System.out.println("top_level_post_id: " + map.get("top_level_post_id"));
+//		System.out.println("content_owner_id_new: " + map.get("content_owner_id_new"));
+//		System.out.println("lesson hashtag: " + doc.select(".bo .bt").text());
+//		System.out.println("name member: " + doc.select("meta[property=\"og:title\"]").attr("content"));
+//		System.out.println(doc.select("meta[property=\"og:title\"]").attr("content"));
 //		
 //		String title = doc.select("title").text();
 //		System.out.println(title);
 //		System.out.println(title.charAt(7));
 
-		Firestore db = FirestoreOptions.getDefaultInstance().getService();
+//		Firestore db = FirestoreOptions.getDefaultInstance().getService();
 //		Map<String, Object> data = new HashMap<>();
 //		Map<String, Object> subdata = new HashMap<>();
 //		subdata.put("Lesson 1", "1");
@@ -42,12 +52,12 @@ public class Main {
 //		db.collection("Learned").document("test2").set(data).get().getUpdateTime();
 		
 		// Update an existing document
-		DocumentReference docRef = db.collection("accounts").document("VawkSF0bnmUmu7bMpC80sNn6Qft1");
-		Map<String, Object> data = new HashMap<>();
-		data.put("Lesson 1", "1");
-		data.put("Lesson 2", "2");
-		// (async) Update one field
-		ApiFuture<WriteResult> future = docRef.update("numLesson", data);
+//		DocumentReference docRef = db.collection("accounts").document("VawkSF0bnmUmu7bMpC80sNn6Qft1");
+//		Map<String, Object> data = new HashMap<>();
+//		data.put("Lesson 1", "1");
+//		data.put("Lesson 2", "2");
+//		// (async) Update one field
+//		ApiFuture<WriteResult> future = docRef.update("numLesson", data);
 
 	}
 
