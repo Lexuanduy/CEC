3ed12485b08edfcc5bb9169027e1a61f85214bf1
lexuanduy <lexuanduy07@gmail.com>
2019-03-28 17:21:27 +0700

    convert url video to https://m.facebook/...

--- src/main/webapp/js/lesson.js
+++ src/main/webapp/js/lesson.js
@@ -1,50 +1,77 @@
-var lessonNumber = $.cookie("numLesson");
 var uid = null;
-$('#nextLesson').click(function() {
-	uid = getCookie('uid');
-	if (uid === undefined) {
-		Swal({
-			title : 'Please login before moving on to the next lesson!'
-		});
-		return;
-	}
-	Swal({
-		title : 'Paste your video link below.'
-	});
-	$('#sendVideo').show();
-	$('#urlLesson').show();
-	$('#nextLesson').hide();
-});
+var uri = window.location.pathname;
+var numLesson = uri.slice(8);
 
-$('#sendVideo').click(
-		function() {
-			var urlVideo = $('#lastLesson').val();
-			if (urlVideo == "") {
-				Swal({
-					title : 'Paste enter link your video.!'
+$('#nextLesson')
+		.click(
+				function() {
+					uid = getCookie('uid');
+					console.log("uid: " + uid);
+					if (uid === undefined) {
+						Swal({
+							position : 'center',
+							title : 'Vui lòng đăng nhập trước khi chuyển sang bài học tiếp theo!',
+							showConfirmButton : false,
+							timer : 2000,
+						});
+						return;
+					}
+					Swal({
+						title : 'Paste your video link below.'
+					});
+					Swal({
+						position : 'center',
+						title : 'Vui lòng nhập link video của bạn về bài học này!',
+						showConfirmButton : false,
+						timer : 2000,
+					});
+					$('#sendVideo').show();
+					$('#urlLesson').show();
+					$('#nextLesson').hide();
 				});
-				return;
-			}
-			uid = getCookie('uid');
-			$.ajax({
-				url : "/checkVideo?url=" + urlVideo + "&uid=" + uid
-						+ "&lessonNumber=" + lessonNumber,
-				type : 'POST',
-				success : function(data) {
-					console.log('data: ' + data);
-					 window.location.href = data;
-				},
-				error : function(jqXHR, textStatus, errorThrown) {
-					if (jqXHR.status == 404) {
-						alert("error 404");
-					}
-					if (jqXHR.status == 403) {
-						alert("error 403");
-					}
 
-					if (jqXHR.status == 405) {
-						alert("error 405");
+$('#sendVideo')
+		.click(
+				function() {
+					var urlVideo = $('#lastLesson').val();
+					if (urlVideo == "") {
+						Swal({
+							position : 'center',
+							title : 'Vui lòng nhập video bài học của bạn trước khi chuyển sang bài học tiếp theo!',
+							showConfirmButton : false,
+							timer : 2000,
+						});
+						return;
 					}
-				}
-			});
-		});
+					var strUrlCut = urlVideo.slice(8,(urlVideo.indexOf("facebook") - 1));
+					var strUrlLast = urlVideo.slice((urlVideo.indexOf("facebook") - 1));
+					var strHTTP = "https://m";
+					var URL = strHTTP + strUrlLast;
+					console.log("URL: " + URL);
+					$.ajax({
+								url : "/checkVideo?url=" + URL + "&numLesson="
+										+ numLesson,
+								type : 'POST',
+								success : function(data) {
+									console.log('data: ' + data);
+									window.location.href = data;
+								},
+								error : function(jqXHR, textStatus, errorThrown) {
+									if (jqXHR.status == 404) {
+										Swal({
+											position : 'center',
+											type : 'error',
+											title : 'Link video không đúng .Vui lòng nhập lại link video bài học của bạn!',
+											showConfirmButton : false,
+											timer : 2000,
+										});
+									}
+									if (jqXHR.status == 403) {
+										alert("error 403");
+									}
+									if (jqXHR.status == 405) {
+										alert("error 405");
+									}
+								}
+							});
+				});
